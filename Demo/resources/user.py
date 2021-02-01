0b84fdeef07acfd1dd68c8d4d8950a3818c2f4c8
ZHchen8397 <zhchen8397@gmail.com>
2021-02-01 18:44:59 +0800

    add __init__.py

--- Demo/resources/user.py
+++ Demo/resources/user.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+
 from common.db import db
 from models.user import UserModel
 
---
+++ Demo/tests/unit_test/test_userModel.py
@@ -0,0 +1,7 @@
+from models.user import UserModel
+import unittest
+
+
+class TestUserModel(unittest.TestCase):
+    def test_get_user(self):
+        self.assertTrue(True)
